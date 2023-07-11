6a40c31fbed118e6a7ec08f672acdddc91d63389
Nastya N <asrozova@gmail.com>
2023-07-12 00:07:24 +0200

    extracommit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header"
 import Modal from "../modal/modal";
-import Overlay from "../modal/overlay/overlay";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { getArray } from '../api/api'

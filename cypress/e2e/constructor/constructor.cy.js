e5d3e9b21c72287e5098f778a17fa403965f4f40
Nastya N <asrozova@gmail.com>
2023-09-01 23:28:16 +0200

    feat:cy_test

--- cypress/e2e/constructor/constructor.cy.js
+++ cypress/e2e/constructor/constructor.cy.js
@@ -1,5 +1,9 @@
 describe('Тестирование работы главной страницы', () => {
     beforeEach(() => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false;
+        });
+
         cy.visit('http://localhost:3000/');
     })
     it('Открытие страницы', () => {
@@ -12,15 +16,12 @@ describe('Тестирование работы главной страницы'
         cy.get('[class^=dnd-container_sections]').should('exist');
 
         cy.get('[id=total_price]').should('have.text', '0');
-        cy.get('button').contains('Оформить заказ').should('exist').and('have.prop', 'disabled');
-
-        // Есть ли на странице секция с ингредиентами?
-        // Есть ли на странице секция с конструктором?
-        
+        cy.get('button').contains('Оформить заказ').should('exist').and('be.disabled');
     })
     it('Секция с ингредиентами', () => {
         cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' });
 
+        cy.get('[id=ingredients_list] li li a').should('exist');
         cy.get('[id=buns_section]').should('exist');
         cy.get('[id=buns_section] li a').should('exist').and('have.prop', 'type', 'bun');
         cy.get('[id=sauces_section]').should('exist');
@@ -28,37 +29,118 @@ describe('Тестирование работы главной страницы'
         cy.get('[id=mains_section]').should('exist');
         cy.get('[id=mains_section] li a').should('exist').and('have.prop', 'type', 'main');
 
-        // Есть ли на странице ингредиенты?
-        // Сортируются ли ингредиенты?
-        // Есть ли в конструкторе ингредиенты?
-        // Меняются ли вкладки припрокрутке???
+        cy.get('[id=buns_section]').scrollIntoView();
+        cy.get('[id=tabs_section]').contains('Булки').parent().should('have.class', 'tab_type_current');
+        cy.get('[id=sauces_section]').scrollIntoView();
+        cy.get('[id=tabs_section]').contains('Соусы').parent().should('have.class', 'tab_type_current');
+        cy.get('[id=mains_section]').scrollIntoView();
+        cy.get('[id=tabs_section]').contains('Начинки').parent().should('have.class', 'tab_type_current');
+
     })
-    // it('Перетаскивание ингредиентов', () => {
-    //     // Перетаскиваются ли элементы?
-    //     // Добавляются ли элементы в конструктор?
-    //     // Сортируются ли элементы булка/начинка?
-    //     // Меняется ли сумма заказа?
-    //     // Активируется ли кнопка при перетаскивании булки?
-    //     // Активируется ли кнопка при перетаскивании начинок без булок?
-    // })
-    // it('Сортировка внутри конструктора', () => {
-    //     // Перетаскиваются ли элементы?
-    //     // Меняется ли порядок ингредиентов в заказе?
-    //     // Меняется ли сумма в заказе?
-    // })
-    // it('Удаление элементов из списка', () => {
-    //     // Есть ли кнопка удалить элементы на начинке?
-    //     // Есть ли кнопка удалить элементы на булке?
-    //     // Удаляются ли элементы?
-    //     // Перестает ли быть активной кнопка,  когда удаляются все элементы инет булки?
-    //     // Меняется ли сумма заказа при удалении?
-    // })
-    // it('Модальные окна', () => {
-    //     // Можно ли нажать на ингредиент?
-    //     // Открывается ли модальное окно при нажатии на ингредиент?
-    //     // Можно ли нажать на кнопку заказа?
-    //     // Проверка авторизации при нажатии на кнопку заказа.
-    //     // Приходят ли данные о заказе(wait)?
-    //     // Открывается ли модальное окно?
-    // })
+    it('Перетаскивание ингредиентов', () => {
+        cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' });
+
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').get('.counter').should('exist').and('have.text', '1');
+        cy.get('[class^=middle_middle]').contains('Соус фирменный Space Sauce');
+        cy.get('button').contains('Оформить заказ').should('be.disabled');
+        cy.get('[id=total_price]').should('have.text', '80');
+
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').get('.counter').should('exist').and('have.text', '2');
+        cy.get(':nth-child(1) > .constructor-element').contains('Соус фирменный Space Sauce');
+        cy.get(':nth-child(2) > .constructor-element').contains('Соус фирменный Space Sauce');
+        cy.get('[id=total_price]').should('have.text', '160');
+
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > [class^=ingredient_card]').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > [class^=ingredient_card] > .counter').should('exist').and('have.text', '1');
+        cy.get('#constructor_list > :nth-child(1) > .constructor-element').contains('Краторная булка N-200i');
+        cy.get('#constructor_list > :nth-child(1) > .constructor-element').not('Соус фирменный Space Sauce');
+        cy.get('[class^=middle_middle]').not('Краторная булка N-200i');
+        cy.get('#constructor_list > :nth-child(3) > .constructor-element').contains('Краторная булка N-200i');
+        cy.get('#constructor_list > :nth-child(3) > .constructor-element').not('Соус фирменный Space Sauce');
+        cy.get('button').contains('Оформить заказ').should('not.be.disabled');
+        cy.get('[id=total_price]').should('have.text', '2670');
+
+        cy.get('#buns_section > .mt-6 > :nth-child(2) > [class^=ingredient_card]').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('#buns_section > .mt-6 > :nth-child(2) > [class^=ingredient_card] > .counter').should('exist').and('have.text', '1');
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > [class^=ingredient_card] > .counter').should('not.exist');
+        cy.get('[id=constructor_list]').contains('Флюоресцентная булка R2-D3');
+        cy.get('[id=constructor_list]').not('Краторная булка N-200i');
+        cy.get('[id=total_price]').should('have.text', '2136');
+    })
+    it('Удаление элементов из списка', () => {
+        cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' });
+
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('[id=ingredients_list] li li a').contains('Соус фирменный Space Sauce').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('[id=ingredients_list] li li a').contains('Биокотлета из марсианской Магнолии').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+
+        cy.get('[id=constructor_list]').contains('Соус фирменный Space Sauce');
+        cy.get('[class^=middle_middle] > :nth-child(1) > .constructor-element > .constructor-element__row > .constructor-element__action > svg')
+            .click();
+        cy.get('[id=total_price]').should('have.text', '504');
+        cy.get('[class^=middle_middle] > :nth-child(2) > .constructor-element > .constructor-element__row > .constructor-element__action > svg')
+            .click();
+        cy.get('[id=constructor_list]').not('Биокотлета из марсианской Магнолии');
+        cy.get('[id=constructor_list]').contains('Соус фирменный Space Sauce');
+    })
+    it('Модальные окна', () => {
+        cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' });
+        cy.intercept('POST', '/api/auth/login', { fixture: 'login.json' });
+        cy.intercept('POST', '/api/orders', { fixture: 'orders.json' });
+        cy.intercept('GET', '/api/auth/user', { fixture: 'user.json' });
+
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > .ingredient_card__ajTps').click();
+        cy.location('pathname').should('eq', '/ingredients/643d69a5c3f7b9001cfa093c');
+        cy.get('[class^=modal_modal]').should('exist');
+        cy.get('[class^=ingredient-delails_contain]').should('exist');
+
+        cy.get('body').type('{esc}');
+        cy.get('[class^=modal_modal]').should('not.exist');
+        cy.location('pathname').should('eq', '/');
+
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > .ingredient_card__ajTps').click();
+        cy.get('[class^=overlay_overlay]').click(15, 15, {force: true});
+        cy.get('[class^=modal_modal]').should('not.exist');
+        cy.location('pathname').should('eq', '/');
+
+        cy.get('#buns_section > .mt-6 > :nth-child(1) > .ingredient_card__ajTps').click();
+        cy.get('[class^=modal_modal] > button').click();
+        cy.get('[class^=modal_modal]').should('not.exist');
+        cy.location('pathname').should('eq', '/');
+
+        cy.get('#buns_section > .mt-6 > :nth-child(2) > [class^=ingredient_card]').trigger('dragstart');
+        cy.get('[id=constructor_list]').trigger('drop');
+        cy.get('button').contains('Оформить заказ').click();
+        cy.location('pathname').should('eq', '/login');
+        cy.get('[class^=login_login_form_container]').should('exist');
+
+        cy.get('input').first().type('asrozova@gmail.com');
+        cy.get('input').last().type('111111');
+        cy.get('button').contains('Войти').click();
+        cy.wait(1500);
+        cy.get('[class^=modal_modal]').should('exist');
+        cy.get('[class^=order-details_order_ditails]').should('exist');
+
+        cy.get('body').type('{esc}');
+        cy.get('[class^=modal_modal]').should('not.exist');
+
+        cy.get('button').contains('Оформить заказ').click();
+        cy.wait(1500);
+        cy.get('[class^=overlay_overlay]').click(30, 30, {force: true});
+        cy.get('[class^=modal_modal]').should('not.exist');
+
+        cy.get('button').contains('Оформить заказ').click();
+        cy.wait(1500);
+        cy.get('[class^=modal_modal] > button').click();
+        cy.get('[class^=modal_modal]').should('not.exist');
+    })
 })
